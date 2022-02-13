06e2beddf3bde12f11ae033a55cdb3a6a7a6fdc3
Vladimir Ivanov <Ivanov.Vladimir@student.greenriver.edu>
2022-02-13 01:55:08 -0500

    chapter 3

---
+++ chapter3/script.js
@@ -0,0 +1,71 @@
+//Assignment: Chapter 3 Practice
+//Author: Vladimir Ivanov
+//Date: 02/01/22
+
+window.onload = () => {
+
+  let button = document.querySelector("button");
+  let inputField = document.querySelector("input");
+  let input = inputField.value;
+  button.addEventListener(
+    "click",
+    () => {
+      printGibberish(input);
+    },
+    false
+  );
+
+ heeHawRecursive(15);
+  
+};
+
+function printGibberish(num) {
+  if (num < 0){
+    let error = document.createElement('li');
+    let errorMessage = document.createTextNode("The number cannot be negative");
+    let position = document.querySelectorAll("ul")[0];
+
+    position.className = "err";
+    error.appendChild(errorMessage);
+    position.appendChild(error);
+  } 
+else {
+  for (let i = 0; i <= num; ++i) {
+    let textReturned = heeHaw(i);
+    let position = document.querySelector("ul");
+    let newText = document.createTextNode(textReturned);
+    let newElement = document.createElement("li"); 
+    newElement.appendChild(newText);
+    position.appendChild(newElement);
+  }
+}
+}
+
+function heeHaw(count) {
+  let text = "";
+  for (let i = 1; i <= count; i++) {
+    i % 3 == 0 && i % 5 == 0
+      ? (text = "Hee, Haw!")
+      : i % 3 == 0
+      ? (text = "Hee!")
+      : i % 5 == 0
+      ? (text = "Haw!")
+      : (text = " " + i);
+  }
+  return text;
+}
+
+function heeHawRecursive(count) {
+  
+  if (count === 0) return;
+
+  count % 3 == 0 && count % 5 == 0
+    ? (console.log( "Hee, Haw!"))
+    : count % 3 == 0
+    ? (console.log( "Hee!"))
+    : count % 5 == 0
+    ? (console.log( "Haw!"))
+    : (console.log( count));
+
+   heeHawRecursive(count - 1);
+}
